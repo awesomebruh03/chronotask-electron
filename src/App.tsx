400e724608c70bdc2d15ef9253dd7e43c08f85c8
Tanzina Ahmed <bhuiyantrina691@gmail.com>
2024-12-10 03:18:52 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MainDashBoard from "./components/Layout/MainDashBoard.tsx";
+
+function App() {
+  return (
+    <>
+    <MainDashBoard/>
+    </>
+  )
+}
+
+export default App
